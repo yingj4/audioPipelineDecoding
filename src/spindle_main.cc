93afc57eaee9f878c8c75be232d34ce610d4d1fa
Samuel Grayson <sam@samgrayson.me>
2020-04-17 02:42:48 -0500

    WIP

--- src/spindle_main.cc
+++ src/spindle_main.cc
@@ -1,9 +1,12 @@
-#include <audio.h>
-#include <common/threadloop.hh>
-#include <common/phonebook.hh>
-#include <common/switchboard.hh>
+#include <chrono>
+#include <thread>
+#include "common/switchboard.hh"
+#include "common/component.hh"
+#include "common/data_format.hh"
+#include "audio.h"
 
 using namespace ILLIXR;
+using namespace ILLIXR_AUDIO;
 
 const size_t TIME_SAFETY_FACTOR = 100;
 
@@ -15,10 +18,10 @@ static void t_reliable_sleep(const std::chrono::duration<Rep, Period>& sleep_dur
 	}
 }
 
-class audio_comp : threadloop {
+class spindle_main : public component {
 public:
-	audio_comp(phonebook* pb)
-		: sb{pb->lookup_impl<switchboard>()}
+	spindle_main(switchboard* sb_)
+		: sb{sb_}
 		, pose{sb->subscribe_latest<pose_type>("slow_pose")}
 		, now{std::chrono::system_clock::now()}
 		, orig_pos{pose->get_latest_ro()->position}
@@ -26,28 +29,31 @@ public:
 	{ }
 
 protected:
-	virtual void _p_one_iteration() {
+	virtual void _p_compute_one_iteration() override {
 		ABAudio::ProcessType procType {ABAudio::ProcessType::FULL};
 		const int numBlocks = 1024;
 		ABAudio audio {"output.wav", procType};
 		audio.loadSource();
 		for (int i = 0; i < numBlocks; ++i) {
-			auto rot = pose->get_latest_ro()->rotation * orig_rot;
+			auto rot = pose->get_latest_ro()->orientation * orig_rot;
 			auto euler_rot = rot.toRotationMatrix().eulerAngles(0, 1, 2);
-			audio.updateRotation(Orientation{
-				euler_rot[0], euler_rot[1], euler_rot[2]});
+			audio.updateRotation(Orientation{static_cast<float>(euler_rot[0]),
+											 static_cast<float>(euler_rot[1]),
+											 static_cast<float>(euler_rot[2])});
 			audio.processBlock();
-			now += std::chrono::milliseconds {43};
-			t_reliable_sleep(now - std::chrono::system_clock::now());
+			now += std::chrono::milliseconds {21};
+			t_reliable_sleep(now - std::chrono::high_resolution_clock::now());
 		}
 	}
 
 private:
 	switchboard * const sb;
-	std::unique_ptr<writer<pose_type>> pose;
+	std::unique_ptr<reader_latest<pose_type>> pose;
 	std::chrono::time_point<std::chrono::system_clock> now;
 	Eigen::Vector3f orig_pos;
 	Eigen::Quaternionf orig_rot;
 };
 
-PLUGIN_MAIN(audio_comp)
+extern "C" component* create_component(switchboard* sb) {
+	return new spindle_main {sb};
+}
